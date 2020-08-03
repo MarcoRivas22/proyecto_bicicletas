8bece7e9dc88f4fa2c7b006d55a297f410209ac7
Marco Rivas <mo.rivasromo@ugto.mx>
2020-08-02 19:11:08 -0500

    Api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,16 @@
-var mymap = L.map('main_map').setView([20.67675, -101.35628], 13);
+var map = L.map('main_map').setView([20.67675, -101.35628], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+}).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
